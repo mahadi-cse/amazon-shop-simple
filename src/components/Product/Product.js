47ff8169767fd2016fe20a357f4e2ac84642fb91
mahadinina <mh952219@gmail.com>
2024-07-06 17:53:04 +0600

    Product Display with image,icon Dynamically

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee , faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    const { img, name ,seller, price,stock} = props.product;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className='info'>
+                <h4 className='name'>{name}</h4>
+                <p>by : {seller}</p>
+                <p>${price}</p>
+                <p>Stock : {stock}</p>
+                <button className='main-button'> <FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData/products.json'
 import './Shop.css'
+import Product from '../Product/Product';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
-    const [products, setProducts] =useState(first10);
+    const [products, setProducts] = useState(first10);
     console.log(first10);
     return (
-        <div>
-            <ul>
-                {products.map(product => <li> {product.name} </li>)}
-            </ul>
+        <div className='shop-container'>
+            <div className="product-container">
+                <ul>
+                    {products.map(pd => <Product product={pd}></Product>)}
+                </ul>
+            </div>
+            <div className="cart-container">
+                <h2>Shop cart</h2>
+            </div>
         </div>
     );
 };

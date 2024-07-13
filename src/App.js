b75e6d14dbe954ca81e1c2e780b96a395cb10895
mahadinina <mh952219@gmail.com>
2024-07-13 17:00:18 +0600

    React router added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Order from './components/Order/Order';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/Shop/NotFound';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Shop />,
+  },
+  {
+    path: '/shop',
+    element: <Shop />,
+  },
+  {
+    path: '/order',
+    element: <Order />,
+  },
+  {
+    path: '/manage',
+    element: <Inventory />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
-     <Shop></Shop>
-
+      <Header />
+      <RouterProvider router={router} />
     </div>
   );
 }
